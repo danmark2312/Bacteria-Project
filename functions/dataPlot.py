0de812c0fb0ca13aa943cab0150e0644aff4ed51
danmark2312 <moe.simon@gmail.com>
2017-11-02 17:27:48 +0100

    Working plots

--- functions/dataPlot.py
+++ functions/dataPlot.py
@@ -8,7 +8,6 @@ Created on Thu Nov  2 15:25:00 2017
 import matplotlib.pyplot as plt
 import matplotlib as mpl
 import numpy as np
-from cycler import cycler
 
 mpl.style.use('ggplot') #Use a prettier plot
 
@@ -17,7 +16,7 @@ def dataPlot(data):
     #Plot "Number of bacteria"
     x,y = np.unique(data[:,2],return_counts=True) #get x and y values
     
-    xLabel = ["Salmonella enterica","Bacillus cereus","Listeria",
+    bacStr = ["Salmonella enterica","Bacillus cereus","Listeria",
              "Brochothrix thermosphacta"] #x-labes
     
     prop_iter = iter(plt.rcParams['axes.prop_cycle']) #Iterate through colors
@@ -25,11 +24,31 @@ def dataPlot(data):
     for i in range(0,len(x)):
         plt.bar(x[i],y[i],color=next(prop_iter)['color']) #Plot bar plot with colors
     
-    plt.xticks(x,xLabel,rotation=35) #Set labes and rotation
+    plt.xticks(x,bacStr,rotation=35) #Set labes and rotation
     
     plt.title("Number of bacteria") #Set title
     
-    plt.show()
+    plt.show() #Show
     
     
-    #Plot "
+    #Plot "Growth rate by temperature"
+  
+    dataSort = data[data[:,0].argsort()] #Sort data for temperature
+    
+    #Plot different graphs for each bacteria
+    for bac in range(len(np.unique(data[:,2]))):
+        
+        mat = dataSort[np.where(dataSort[:,2] == bac+1)] #Define matrix of bacteria
+
+        #Check if matrix is not empty
+        if len(mat) != 0:    
+            x,y = mat[:,0],mat[:,1] #get x and y axis
+        
+            plt.plot(x,y, label=bacStr[bac]) #plot graph
+    
+    plt.legend(bbox_to_anchor=(1.05, 0.65), loc=2, borderaxespad=0.) #Set legend
+    
+    plt.title("Growth rate by temperature") #Set title
+    
+    plt.show() #Show
+    
