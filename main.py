53c81e4feffc3b79fed32cf4424ef5ad771afaec
danmark2312 <moe.simon@gmail.com>
2017-11-02 17:28:35 +0100

    Removed already defined function

--- main.py
+++ main.py
@@ -13,24 +13,3 @@ from functions.userinput import displayMenu,inputNumber,inputStr
 
 
 dataLoad("test.txt")
-
-
-# Following code is copy-pasted as a frame/skabelon
-def displayMenu(options): 
-# DISPLAYMENU Displays a menu of options, ask the user to choose an item 
-# and returns the number of the menu item chosen.
-# Usage: choice = displayMenu(options) 
-# 
-# Input options Menu options (array of strings) 
-# Output choice Chosen option (integer) 
-# 
-#
-# Display menu options 
-for i in range(len(options)):
-    print("{:d}. {:s}".format(i+1, options[i]))
-#
-# Get a valid menu choice
-choice = 0
-while not(np.any(choice == np.arange(len(options))+1)):
-    choice = inputNumber("Please choose a menu item: ")
-return choice
