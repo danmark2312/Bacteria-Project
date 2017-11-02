3673055dcc1beb9993c28b7bcfd6163d5c7c2b29
danmark2312 <moe.simon@gmail.com>
2017-11-02 15:22:40 +0100

    Used enumerate instead of line variable

--- functions/dataLoad.py
+++ functions/dataLoad.py
@@ -17,32 +17,30 @@ def dataLoad(filename):
     
     #Initial variables
     data = [0,0,0]
-    line = 1
     read = True
     
     #Append each row to dataRaw
     with open(filename, newline='') as inputfile:
-        for row in csv.reader(inputfile):
+        for line,row in enumerate(csv.reader(inputfile)):
             
             arr = np.array(row[0].split(" "), dtype=float) #Creating an array of row
             
             #Checking for error conditions
             if ((10 > arr[0]) or (arr[0] > 60)): #Temperature
                 read = False #Do not read line
-                print("Erroneous line at line:",line,"Temperature did not meet requirements")
+                print("Erroneous line at line:",line+1,"Temperature did not meet requirements")
                 
             if (arr[1]<0): #Growth rate
                 read = False #Do not read line
-                print("Erroneous line at line:",line,"Growth rate did not meet requirements")
+                print("Erroneous line at line:",line+1,"Growth rate did not meet requirements")
                 
             if ((1 >= arr[0]) and (arr[0] <= 4)): #Bacteria type
                 read = False #Do not read line
-                print("Erroneous line at line:",line,"Bacteria type did not meet requirements")
+                print("Erroneous line at line:",line+1,"Bacteria type did not meet requirements")
             
             if read:
                 data = np.vstack((data,arr)) #Stack row into N x 3 matrix
-                
-            line += 1 #Add one to line           
+          
             read = True #reset read
     
     data = data[1:len(data)] #Remove placeholder of [0,0,0]
