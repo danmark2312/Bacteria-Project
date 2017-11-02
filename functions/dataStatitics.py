f6c087f0a81d0fac5c228dfe56535c7210f7b434
emilballermann <emil_ballermann@hotmail.com>
2017-11-02 16:22:30 +0100

    Final statistic function, checked

--- functions/dataStatitics.py
+++ functions/dataStatitics.py
@@ -6,20 +6,27 @@ Created on Thu Nov  2 15:25:01 2017
 """
 
 def dataStatistics(data,statistics):
+#Defining average temperature
     if statistics == "Mean Temperature":
         result=np.mean(data[:,0])
+#Defining average growth rate
     elif statistics == "Mean Growth rate":
         result=np.mean(data[:,1])
+#Defining standard deviation of temperature
     elif statistics == "Std Temperature":
         result=np.std(data[:,0])
+#Defining standard deviation of growth rate 
     elif statistics == "Std Growth rate":
         result=np.std(data[:,1])
+#Counting numbers of rows in data
     elif statistics == "Rows":
         result=len(data)
+#Defining average growth rate for every data under 20 degrees
     elif statistics == "Mean Cold Growth rate":
-        if data[:,0]<20:
-            result=np.mean(data[:,1])
+        index1 = np.where(data[:,0]<20)
+        result = np.mean(data[index1,1])
+#Defining average growth rate for every data above 50 degrees
     elif statistics == "Mean Hot Growth rate":
-        if data[:,0]>50:
-            result=np.mean(data[:,1])
+        index2 = np.where(data[:,0]>50)
+        result=np.mean(data[index2,1])
     return result
