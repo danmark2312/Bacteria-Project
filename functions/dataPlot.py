18c810a11dacb967f9aff4a8f922799178fa52a6
danmark2312 <moe.simon@gmail.com>
2017-11-02 16:01:15 +0100

    Added first plot

--- functions/dataPlot.py
+++ functions/dataPlot.py
@@ -5,6 +5,27 @@ Created on Thu Nov  2 15:25:00 2017
 @author: s174420
 """
 
+import matplotlib.pyplot as plt
+import matplotlib
+import numpy as np
+
+matplotlib.style.use('ggplot') #Use a prettier plot
+
 def dataPlot(data):
+    
+    #Plot "Number of bacteria"
+    x,y = np.unique(data[:,2],return_counts=True) #get x and y values
+    
+    xLabel = ["Salmonella enterica","Bacillus cereus","Listeria",
+             "Brochothrix thermosphacta"] #x-labes
+    
+    plt.bar(x,y) #Plot bar plot
+    
+    plt.xticks(x,xLabel,rotation=45)
+    
+    plt.show()
+    
+    
+    
     
     
