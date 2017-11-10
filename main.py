a9b219a0dad17a4c35dd82cf13036a419e46c6c3
danmark2312 <moe.simon@gmail.com>
2017-11-10 19:29:55 +0100

    Added better messages

--- main.py
+++ main.py
@@ -7,8 +7,8 @@ COMMENT ON MAIN SCRIPT
 from functions.dataLoad import dataLoad
 from functions.dataPlot import dataPlot
 from functions.dataStatistics import dataStatistics
-from functions.userinput import displayMenu,inputStr
-from functions.filterData import filterData
+from functions.userinput import displayMenu,inputStr,header
+from functions.filterData import filterData,printFilter
 import numpy as np
 
 #Add welcoming message
@@ -16,26 +16,31 @@ import numpy as np
 
 #Initial variables
 dataLoaded = False
-
+bacActive = "No active bacteria filter"
+rangeActive = "No active range filter"
 
 #Keep menu until user quits
 while True:
+    #Print any active filters
+    printFilter(bacActive,rangeActive)
+    header("MAIN MENU") #Interface
     #User Menu
     menu = displayMenu(["Load data","Filter data","Display statistics", "Generate plots", "Quit"])
     
     #Load data
     if menu == 1:
         #Check for correct filename
-        print("\nIf you wish to exit, type 'exit'")
-        
-        while not dataLoaded:
+        header("LOAD DATA MENU")
+        print("If you wish to exit, type 'exit'")
+        while True:
             try:
                 filename = inputStr("Please enter the name of the datafile: ")
+                print("") #Add space
                 
                 #Check for exit
                 if filename != "exit":
                     data = dataLoad(filename) #Load data
-                    print("\nData loaded succesfully from",filename,"\n")
+                    print("\nData loaded succesfully from",filename)
                     dataLoaded = True #Set data as loaded
                     dataOld = np.copy(data) #Copy of data
                     break
@@ -50,16 +55,20 @@ while True:
     #Filter data
     elif (menu == 2) and dataLoaded:
         while True:
-            print("\nPlease specify a filter")
+            header("FILTER MENU")
+            print("Please specify a filter")
+            #Print any active filters
+            printFilter(bacActive,rangeActive)
+            
             menu2 = displayMenu(["Bacteria filter","Range filter","Back"])
             
             #Bacteria type filter
             if menu2 == 1:
-                data,bacActive,rangeActive = filterData("Bacteria filter",dataOld)
+                data,bacActive,rangeActive = filterData("Bacteria filter",dataOld,bacActive,rangeActive)
                
             #Range filter
             elif menu2 == 2:
-                data,bacActive,rangeActive = filterData("Range filter",dataOld)
+                data,bacActive,rangeActive = filterData("Range filter",dataOld,bacActive,rangeActive)
             
             #Back
             elif menu2 == 3:
@@ -71,7 +80,10 @@ while True:
         statStr = ["Mean Temperature","Mean Growth rate","Std Temperature","Std Growth rate",
                              "Rows","Mean Cold Growth rate","Mean Hot Growth rate","Back"]
         
-        while True:            
+        header("STATISTICS MENU")
+        while True:   
+            #Print any active filters
+            printFilter(bacActive,rangeActive)
             menu2 = displayMenu(statStr) #Show different statistics to be computed
             if menu2 == 8: #Quit
                 break
@@ -79,14 +91,14 @@ while True:
                 stat = dataStatistics(data,statStr[int(menu2-1)]) #Compute statistic
             
             #Print statistic
-            print("""
-==================================================   
+            print("""\n==================================================   
 {} | {}
-==================================================""".format(statStr[int(menu2-1)],stat))
+==================================================\n""".format(statStr[int(menu2-1)],stat))
             
     #Generate plots
     elif (menu == 4) and dataLoaded:
         dataPlot(data)
+        print("")
     
     #Break
     elif menu == 5:
