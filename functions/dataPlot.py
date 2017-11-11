2c966ae777a0f0669c7f6cc488f71dfc10a9e0c2
Simon Moe <moe.simon@gmail.com>
2017-11-11 01:20:17 +0100

    Added working plot

--- functions/dataPlot.py
+++ functions/dataPlot.py
@@ -25,19 +25,15 @@ def dataPlot(data):
     
     #Plot "Number of bacteria"
     x,y = np.unique(data[:,2],return_counts=True) #get x and y values
-    
     bacStr = ["Salmonella enterica","Bacillus cereus","Listeria",
              "Brochothrix thermosphacta"] #x-labes
-    
     prop_iter = iter(plt.rcParams['axes.prop_cycle']) #Iterate through colors
     
     for i in range(0,len(x)):
         plt.bar(x[i],y[i],color=next(prop_iter)['color']) #Plot bar plot with colors
     
     plt.xticks(x,[bacStr[int(i)-1] for i in x],rotation=35) #Set labes and rotation
-    
     plt.title("Number of bacteria") #Set title
-    
     plt.show() #Show
     
     
@@ -47,17 +43,12 @@ def dataPlot(data):
     
     #Plot different graphs for each bacteria
     for bac in np.unique(data[:,2]):
-        
-        mat = dataSort[np.where(dataSort[:,2] == int(bac)-1)] #Define matrix of bacteria
-
+        mat = dataSort[np.where(dataSort[:,2] == int(bac))] #Define matrix of bacteria
         #Check if matrix is not empty
         if len(mat) != 0:    
             x,y = mat[:,0],mat[:,1] #get x and y axis
-        
             plt.plot(x,y, label=bacStr[int(bac)-1]) #plot graph
     
     plt.legend(bbox_to_anchor=(1.05, 0.65), loc=2, borderaxespad=0.) #Set legend
-    
     plt.title("Growth rate by temperature") #Set title
-    
     plt.show() #Show
