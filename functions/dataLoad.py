74b6c279be4393f7d1c9b713c402a0d179ff0fcf
danmark2312 <moe.simon@gmail.com>
2017-11-16 14:00:37 +0100

    Added GUI for 50% of the program

--- functions/dataLoad.py
+++ functions/dataLoad.py
@@ -21,7 +21,7 @@ def dataLoad(filename):
     #Initial variables
     data = [0,0,0]
     read = True
-    msg = ""
+    msg = []
 
     #Append each row to dataRaw
     with open(filename, newline='') as inputfile:
@@ -33,16 +33,16 @@ def dataLoad(filename):
                 #Checking for error conditions
                 if ((10 >= arr[0]) or (arr[0] >= 60)): #Temperature
                     read = False #Do not read line
-                    msg = msg.append("Erroneous line at line: {} Temperature did not meet requirements".format(line+1))
+                    msg.append("Erroneous line at line: {} Temperature did not meet requirements".format(line+1))
 
                 if (arr[1]<0): #Growth rate
                     read = False #Do not read line
-                    print("Erroneous line at line:",line+1,"Growth rate did not meet requirements")
+                    msg.append("Erroneous line at line: {} Growth rate did not meet requirements".format(line+1))
 
 
                 if ((0 >= arr[2]) or (arr[2] > 4)): #Bacteria type
                     read = False #Do not read line
-                    print("Erroneous line at line:",line+1,"Bacteria type did not meet requirements")
+                    msg.append("Erroneous line at line: {} Bacteria type did not meet requirements".format(line+1))
 
                 if read:
                     data = np.vstack((data,arr)) #Stack row into N x 3 matrix
@@ -52,6 +52,7 @@ def dataLoad(filename):
             else:
                 print("Erroneous line at line:",line+1,"Length of row was not 3")
 
+    msg = "\n".join(msg)
     data = data[1:len(data)] #Remove placeholder of [0,0,0]
 
     return data,msg
--- functions/dataStatistics.py
+++ functions/dataStatistics.py
@@ -12,62 +12,61 @@ def dataStatistics(data,statistics):
             "Mean Growth rate"
             "Std Temperature"
             "Std Growth rate"
-            "Rows"
             "Mean Cold Growth rate"
             "Mean Hot Growth rate"
-            "Minimum values"
-            "Maximum values"
-            "Back"
+            "Min | max Growth rate"
+            "Min | max Temperature"
+            "Rows"
     OUTPUT:
         result = A float representing the given statistic
-        
+
     USAGE:
         stat = dataStatistics(data,statistics)
-        
+
     Emil Ballermann (s174393) & Simon Moe Sørensen (s174420)
     """
     #Check for length of data
     if len(data) != 0:
-            
+
         #Defining average temperature
         if statistics == "Mean Temperature":
             result=np.mean(data[:,0])
-            
+
         #Defining average growth rate
         elif statistics == "Mean Growth rate":
             result=np.mean(data[:,1])
-            
+
         #Defining standard deviation of temperature
         elif statistics == "Std Temperature":
             result=np.std(data[:,0])
-            
-        #Defining standard deviation of growth rate 
+
+        #Defining standard deviation of growth rate
         elif statistics == "Std Growth rate":
             result=np.std(data[:,1])
-            
+
         #Counting numbers of rows in data
         elif statistics == "Rows":
             result=len(data)
-            
+
         #Defining average growth rate for every data under 20 degrees
         elif statistics == "Mean Cold Growth rate":
             index1 = np.where(data[:,0]<20)
             result = np.mean(data[index1,1])
-            
+
         #Defining average growth rate for every data above 50 degrees
         elif statistics == "Mean Hot Growth rate":
             index2 = np.where(data[:,0]>50)
             result=np.mean(data[index2,1])
-        
+
         #Define minimum values for temp and growth rate
-        elif statistics == "Minimum Values":
-            result = "Temperature: {:f} | Growth rate: {:f}".format(min(data[:,0]),min(data[:,1]))
-        
+        elif statistics == "Min | max Growth rate":
+            result = "{:f} | {:f}".format(min(data[:,0]),min(data[:,1]))
+
         #Define maximum values for temp and growth rate
-        elif statistics == "Maximum Values":
-                result = "Temperature: {:f} | Growth rate: {:f}".format(max(data[:,0]),max(data[:,1]))
-                
+        elif statistics == "Min | max Temperature":
+                result = "{:f} | {:f}".format(max(data[:,0]),max(data[:,1]))
+
     else:
         result = np.nan
-    
+
     return result
