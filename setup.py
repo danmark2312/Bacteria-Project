13311b6683f08de5e35df67641b8df51e8707db5
Simon Moe Sørensen <moe.simon@gmail.com>
2017-11-16 22:55:12 +0100

    Added setup script

--- setup.py
+++ setup.py
@@ -10,26 +10,28 @@ USAGE:
 
 IMPORTANT:
     It does not always work on macs due to errors in the source code of cx_freeze
-    
+
 Emil Ballermann (s174393) & Simon Moe Sørensen (s174420)
 """
 
 from cx_Freeze import setup, Executable
 
-# Dependencies are automatically detected, but it might need fine tuning.
-additional_mods = ['numpy.core._methods', 'numpy.lib.format', 
+#Dependencies are automatically detected, but it might need fine tuning.
+additional_mods = ['numpy.core._methods', 'numpy.lib.format',
                    'matplotlib.backends.backend_qt5agg']
+
+include_files = ["functions"]
 
 packages = ["numpy","matplotlib.pyplot","csv"]
 
-build_exe_options = {"packages": packages, "excludes": ["tkinter"], 
-                     "includes":additional_mods}
+build_exe_options = {"packages": packages, "excludes": ["tkinter"],
+                     "includes":additional_mods, "include_files":include_files}
 
-base = None
+base = "Win32GUI"
 
 setup(  name = "Bacteria Data Analysis Project",
         version = "1.0",
         author = "Simon Moe Sørensen & Emil Ballermann",
         description = "This program analyses data from bacteria experiments",
         options = {"build_exe": build_exe_options},
-        executables = [Executable("main.py", base=base)])
+        executables = [Executable("GUI.py", base=base, icon="resources/Icon.ico")])
