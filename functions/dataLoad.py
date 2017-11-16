5549fe51d6e50d0c666d98bd2e55e9307667c3c6
danmark2312 <moe.simon@gmail.com>
2017-11-16 10:49:33 +0100

    Started working on GUI-version of dataLoad

--- functions/dataLoad.py
+++ functions/dataLoad.py
@@ -7,50 +7,51 @@ def dataLoad(filename):
     """
     INPUT:
         filename: A string containing the full filename (with extension) of a datafile
-        
+
     OUTPUT:
         data: An N x 3 matrix
-        
+
     USAGE:
         data = dataLoad(filename)
-        
-    
+
+
     Emil Ballermann (s174393) & Simon Moe Sørensen (s174420)
     """
-    
+
     #Initial variables
     data = [0,0,0]
     read = True
-    
+    msg = ""
+
     #Append each row to dataRaw
     with open(filename, newline='') as inputfile:
         for line,row in enumerate(csv.reader(inputfile)):
-            
+
             arr = np.array(row[0].split(" "), dtype=float) #Creating an array of row
-            #Only read line if len is three 
-            if len(arr) == 3:                
+            #Only read line if len is three
+            if len(arr) == 3:
                 #Checking for error conditions
                 if ((10 >= arr[0]) or (arr[0] >= 60)): #Temperature
                     read = False #Do not read line
-                    print("Erroneous line at line:",line+1,"Temperature did not meet requirements")
-                    
+                    msg = msg.append("Erroneous line at line: {} Temperature did not meet requirements".format(line+1))
+
                 if (arr[1]<0): #Growth rate
                     read = False #Do not read line
                     print("Erroneous line at line:",line+1,"Growth rate did not meet requirements")
-                
-                
+
+
                 if ((0 >= arr[2]) or (arr[2] > 4)): #Bacteria type
                     read = False #Do not read line
                     print("Erroneous line at line:",line+1,"Bacteria type did not meet requirements")
-                
+
                 if read:
                     data = np.vstack((data,arr)) #Stack row into N x 3 matrix
-                
+
                 read = True #reset read
-                
+
             else:
                 print("Erroneous line at line:",line+1,"Length of row was not 3")
-                
+
     data = data[1:len(data)] #Remove placeholder of [0,0,0]
 
-    return data
+    return data,msg
--- functions/filterData.py
+++ functions/filterData.py
@@ -8,124 +8,124 @@ def printFilter(bacActive,rangeActive):
     INPUT:
         bacActive: A list specifying the active bacteria filters, if none
             then it is a string stating there is no active filter of given type
-            
+
         rangeActive: A list specifying the active range filter, if none
             then it is a string stating there is no active filter of given type
-    
+
     OUTPUT:
         Message specifying current filters
     """
-    
-    if (type(bacActive) == np.ndarray) or (type(rangeActive) == list): 
+
+    if (type(bacActive) == np.ndarray) or (type(rangeActive) == list):
     #Print active filters if any
         print("""=======================================================
                      ACTIVE FILTERS
-         
+
 Current filters:
-Bacteria: {} 
+Bacteria: {}
 Range: {}
 =======================================================
                       """.format(bacActive,rangeActive))
-    
+
 
 def filterData(filtertype,data,dataOld,conditions):
     """
     INPUT:
         filtertype: A string specifying how to filter the data
-        
+
         data: Current data
-        
+
         dataOld: Unfiltered data
-    
-        conditions: Collection of filtering information       
-        
+
+        conditions: Collection of filtering information
+
     OUTPUT:
         data: Filtered data
-        
-        conditions: Collection of filtering information  
-        
+
+        conditions: Collection of filtering information
+
     USAGE:
         data,conditions = filterData(filtertype,data,dataOld,conditions)
-    
+
     Emil Ballermann (s174393) & Simon Moe Sørensen (s174420)
     """
     #Initial variables
     bacStr = ["Salmonella enterica","Bacillus cereus","Listeria",
           "Brochothrix thermosphacta"]
     r1,r2 = None,None
-    
+
     #Extracting variables from condition list
     bacActive = conditions[0] #Active bacteria filters
     rangeActive = conditions[1] #Active range filters
     bacList = conditions[2] #Array of bacteria types, integers
     range_ = conditions[3] #Boolean array where range was true
     mask = conditions[4] #Boolean array where bacList is in data
-        
-    #Range filter    
+
+    #Growth rate filter  
     if filtertype == "Range filter":
         header("RANGE FILTER MENU") #Interface
         print("""You have chosen to filter for range.
 Type "clear" to clear range""")
-        
+
         while True:
-            r1 = inputLimit("Please enter a lower limit: ") 
+            r1 = inputLimit("Please enter a lower limit: ")
             #Break if clear
             if r1 == "clear":
                 rangeActive = "No active range filter"
                 break
-            
+
             r2 = inputLimit("Please enter an upper limit: ")
             #Break if clear
             if r2 == "clear":
                 rangeActive = "No active range filter"
                 break
-                
+
             #Get min and max, in case of wrong order
             rangeActive = [min(r1,r2),max(r1,r2)]
             break
-   
+
     #Bacteria filter
     elif filtertype == "Bacteria filter":
         header("BACTERIA FILTER MENU") #Interface
         print("""You have chosen to filter for bacteria.
 Select the bacteria you want to filter
 If it is already a filter, it will be removed\n""")
-                
+
         while True:
             printFilter(bacActive,rangeActive) #Print filter
             menu = int(displayMenu(bacStr+["Back"])) #Display a menu
-            
+
             #Back
             if menu == 5:
                 break
-            
+
             #Check if menu (bacteria chosen) is in bacList
             if menu in bacList:
                 bacList = bacList[bacList != menu] #Remove from array
             else:
                 bacList = np.append(bacList,menu) #Add to array
-            
+
             bacActive = np.array(bacStr)[bacList-1] #Active bacteria filter
-            
+
             if len(bacActive) == 0:
-                bacActive = "No active bacteria filter" 
+                bacActive = "No active bacteria filter"
 
     #Use mask and range_ to filter data if filter is active (specific type)
     if type(bacActive) != str: #For bacteria
         mask = np.in1d(dataOld[:,2],bacList) #Where each value of bacList is in dataOld
         data = dataOld[mask] #Masking from unfiltered data
-        
+
     else:
         data = dataOld #Data is the same as old
-    
+
     if type(rangeActive) != str: #For range
-        range_ = ((rangeActive[0] < data[:,1]) & (data[:,1] < rangeActive[1]))   
+        range_ = ((rangeActive[0] < data[:,1]) & (data[:,1] < rangeActive[1]))
         data = data[range_] #Data is filtered for range
-        
+
     if not (type(bacActive) != str) or (type(rangeActive) != str):
         data = data #No changes to data, but it is pre-filtered
-    
+
     #Contain conditions from filter function in list
     conditions = [bacActive,rangeActive,bacList,range_,mask]
-    
+
     return data,conditions
