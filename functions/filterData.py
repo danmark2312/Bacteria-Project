a8dfb763ddea6a0b71cd0046274206439957a345
emilballermann <33318888+emilballermann@users.noreply.github.com>
2017-11-16 14:19:41 +0100

    Old

--- functions/filterData.py
+++ functions/filterData.py
@@ -3,30 +3,29 @@
 from functions.userinput import displayMenu,inputLimit,header
 import numpy as np
 
-def printFilter(bacActive,growthActive,tempActive):
+def printFilter(bacActive,rangeActive):
     """
     INPUT:
         bacActive: A list specifying the active bacteria filters, if none
             then it is a string stating there is no active filter of given type
             
-        growthActive: A list specifying the active range filter, if none
+        rangeActive: A list specifying the active range filter, if none
             then it is a string stating there is no active filter of given type
     
     OUTPUT:
         Message specifying current filters
     """
     
-    if (type(bacActive) == np.ndarray) or (type(growthActive) == list) or (type(tempActive) == list): 
+    if (type(bacActive) == np.ndarray) or (type(rangeActive) == list): 
     #Print active filters if any
         print("""=======================================================
                      ACTIVE FILTERS
          
 Current filters:
 Bacteria: {} 
-Growth rate range: {}
-Temperature range: {}
+Range: {}
 =======================================================
-                      """.format(bacActive,growthActive,tempActive))
+                      """.format(bacActive,rangeActive))
     
 
 def filterData(filtertype,data,dataOld,conditions):
@@ -57,70 +56,34 @@ def filterData(filtertype,data,dataOld,conditions):
     
     #Extracting variables from condition list
     bacActive = conditions[0] #Active bacteria filters
-    growthActive = conditions[1] #Active range filters
-    tempActive = conditions[2]#Active temperature filter
-    bacList = conditions[3] #Array of bacteria types, integers
-    range_ = conditions[4] #Boolean array where range was true
-    mask = conditions[5] #Boolean array where bacList is in data
+    rangeActive = conditions[1] #Active range filters
+    bacList = conditions[2] #Array of bacteria types, integers
+    range_ = conditions[3] #Boolean array where range was true
+    mask = conditions[4] #Boolean array where bacList is in data
         
-    #Growth rate range filter    
-    if filtertype == "Growth rate range filter":
-        header("GROWTH RATE RANGE FILTER MENU") #Interface
-        print("""You have chosen to filter for growth rate range.
-Type "clear" to clear range or "back" to go back""")
-        
-        while True:
-            r1 = inputLimit("Please enter a lower limit: ") 
-            #Break if clear
-            if r1 == "clear":
-                growthActive = "No active range filter"
-                break
-            #Exit without changing anything
-            elif r1 == "back":
-                break
-            
-            r2 = inputLimit("Please enter an upper limit: ")
-            #Break if clear
-            if r2 == "clear":
-                growthActive = "No active range filter"
-                break
-            #Exit without changing anything
-            elif r2 == "back":
-                break
-                
-            #Get min and max, in case of wrong order
-            growthActive = [min(r1,r2),max(r1,r2)]
-            break
-        
-    #Temperature range filter     
-    elif filtertype == "Temperature range filter":
-        header("TEMPERATURE FILTER MENU") #Interface
-        print("""You have chosen to filter for a temperature range.
-Type "clear" to clear range and "back" to go back""")
+    #Range filter    
+    if filtertype == "Range filter":
+        header("RANGE FILTER MENU") #Interface
+        print("""You have chosen to filter for range.
+Type "clear" to clear range""")
         
         while True:
             r1 = inputLimit("Please enter a lower limit: ") 
             #Break if clear
             if r1 == "clear":
-                tempActive = "No active temperature range filter"
-                break
-            #Exit without changing anything
-            elif r1 == "back":
+                rangeActive = "No active range filter"
                 break
             
             r2 = inputLimit("Please enter an upper limit: ")
             #Break if clear
             if r2 == "clear":
-                tempActive = "No active temperature range filter"
-                break
-            #Exit without changing anything
-            elif r2 == "back":
+                rangeActive = "No active range filter"
                 break
                 
             #Get min and max, in case of wrong order
-            tempActive = [min(r1,r2),max(r1,r2)]
+            rangeActive = [min(r1,r2),max(r1,r2)]
             break
-        
+   
     #Bacteria filter
     elif filtertype == "Bacteria filter":
         header("BACTERIA FILTER MENU") #Interface
@@ -129,7 +92,7 @@ Select the bacteria you want to filter
 If it is already a filter, it will be removed\n""")
                 
         while True:
-            printFilter(bacActive,growthActive,tempActive) #Print filter
+            printFilter(bacActive,rangeActive) #Print filter
             menu = int(displayMenu(bacStr+["Back"])) #Display a menu
             
             #Back
@@ -155,18 +118,14 @@ If it is already a filter, it will be removed\n""")
     else:
         data = dataOld #Data is the same as old
     
-    if type(growthActive) != str: #For growth rate range
-        range_ = ((growthActive[0] < data[:,1]) & (data[:,1] < growthActive[1]))   
-        data = data[range_] #Data is filtered for growth rate range
+    if type(rangeActive) != str: #For range
+        range_ = ((rangeActive[0] < data[:,1]) & (data[:,1] < rangeActive[1]))   
+        data = data[range_] #Data is filtered for range
         
-    if type(tempActive) != str: #For temperature range
-        range_ = ((tempActive[0] < data[:,0]) & (data[:,0] < tempActive[1]))   
-        data = data[range_] #Data is filtered for temperature range
-        
-    if not (type(bacActive) != str) or (type(growthActive) != str) or (type(tempActive) != str):
+    if not (type(bacActive) != str) or (type(rangeActive) != str):
         data = data #No changes to data, but it is pre-filtered
     
     #Contain conditions from filter function in list
-    conditions = [bacActive,growthActive,tempActive,bacList,range_,mask]
+    conditions = [bacActive,rangeActive,bacList,range_,mask]
     
     return data,conditions
